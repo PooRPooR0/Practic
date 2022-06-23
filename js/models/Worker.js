3beac99f5f6ba97a2b4832a78addb9bd0a5d335a
Oleg <kombo007@mail.ru>
2022-06-23 10:57:27 +0700

    Deleted screem system

--- js/models/Worker.js
+++ js/models/Worker.js
@@ -4,47 +4,11 @@ import Food from "./Food.js"
 import Wall from "./Wall.js"
 
 export default class Worker extends MovableObject {
-    #screemRadius
-    #footsToFood
-    #footsToHive
     #isMovedToHive
-    #heardScreams = []
-    #distAfterHeardScreem
 
-    constructor(x_pos, y_pos, x_size, y_size, speed, screemRadius) {
+    constructor(x_pos, y_pos, x_size, y_size, speed) {
         super(x_pos, y_pos, x_size, y_size, speed)
-        this.#footsToFood = 0
-        this.#footsToHive = 0
         this.#isMovedToHive = false
-        this.#screemRadius = screemRadius
-        this.#heardScreams = []
-        this.#distAfterHeardScreem = 0
-    }
-
-    set screemRadius(screemRadius) {
-        if (screemRadius >= 0) {
-            this.#screemRadius = screemRadius
-        }
-    }
-
-    get screemRadius() {
-        return this.#screemRadius
-    }
-
-    set footsToFood(footsToFood) {
-        this.#footsToFood = footsToFood
-    }
-
-    get footsToFood() {
-        return this.#footsToFood
-    }
-
-    set footsToHive(footsToHive) {
-        this.#footsToHive = footsToHive
-    }
-
-    get footsToHive() {
-        return this.#footsToHive
     }
 
     set isMovedToHive(isMovedToHive) {
@@ -53,14 +17,6 @@ export default class Worker extends MovableObject {
 
     get isMovedToHive() {
         return this.#isMovedToHive
-    }
-
-    set heardScreams(heardScreams) {
-        this.#heardScreams = heardScreams
-    }
-
-    get heardScreams() {
-        return this.#heardScreams
     }
 
     drawSelf(ctx) {
@@ -81,22 +37,16 @@ export default class Worker extends MovableObject {
 
             if(x_dist > obj.x_size / 2 || y_dist > obj.y_size / 2) continue
             
-            if (obj instanceof Hive) {
-                this.footsToHive = 0
-                if (this.isMovedToHive) {
-                    this.moveDirection = this.moveDirection + 180
-                    this.moveToDirection(this.moveDirection)
-                    this.isMovedToHive = false
-                }
+            if (obj instanceof Hive && this.isMovedToHive) {
+                this.moveDirection = this.moveDirection + 180
+                this.moveToDirection(this.moveDirection)
+                this.isMovedToHive = false
             }
 
-            if (obj instanceof Food) {
-                this.footsToFood = 0
-                if (!this.isMovedToHive) {
-                    this.moveDirection = this.moveDirection + 180
-                    this.moveToDirection(this.moveDirection)
-                    this.isMovedToHive = true
-                }
+            if (obj instanceof Food && !this.isMovedToHive) {
+                this.moveDirection = this.moveDirection + 180
+                this.moveToDirection(this.moveDirection)
+                this.isMovedToHive = true
             }
 
             if (obj instanceof Wall) {
@@ -108,61 +58,12 @@ export default class Worker extends MovableObject {
         }
     }
 
-    screaming(workers) {
-        for (let otherWorker of workers) {
-            if(this.id === otherWorker.id) continue
-            const distance = this.countDistance(otherWorker.x_pos, otherWorker.y_pos)
-            if (distance.line_distance > this.screemRadius) continue
-
-            let footsToScream
-            if (this.isMovedToHive) footsToScream = this.footsToFood + this.screemRadius
-            else footsToScream = this.footsToHive + this.screemRadius
-
-            otherWorker.heardScreams = [...otherWorker.heardScreams, {
-                "dist": footsToScream,
-                "isHeMovedToHive": this.isMovedToHive,
-                "worker": this
-            }]
-        }
-    }
-
-    heard() {
-        if (this.#distAfterHeardScreem > 0) {
-            this.heardScreams = []
-            return
-        }
-
-        for (let scream of this.heardScreams) {
-            if(scream.isHeMovedToHive) {
-                if (scream.dist >= this.footsToFood) continue
-                this.footsToFood = scream.dist
-                    
-                if (this.isMovedToHive) continue
-                this.setDirectionToTarget(scream.worker)
-                this.#distAfterHeardScreem = this.#screemRadius
-            }else {
-                if (scream.dist >= this.footsToHive) continue
-                this.footsToHive = scream.dist
-                
-                if (!this.isMovedToHive) continue
-                this.setDirectionToTarget(scream.worker)
-                this.#distAfterHeardScreem = this.#screemRadius
-            }
-        }
-
-        this.heardScreams = []
-    }
-
     live(foods, hive, workers, walls, ctx) {
         this.moveToDirection(this.moveDirection)
-
-        this.footsToFood = this.footsToFood + this.speed
-        this.footsToHive = this.footsToHive + this.speed
+        
         this.moveDirection = this.moveDirection + Math.random() * 0.2 - 0.1
-        if (this.#distAfterHeardScreem > 0) this.#distAfterHeardScreem -= this.speed
 
         this.collide([...foods, ...walls, hive])
-        this.screaming(workers)
-        this.heard()
+        
     }
 }
